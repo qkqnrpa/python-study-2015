bd086fe0f2c4515d4b09dfdafbb831c9f6bc0489
GimHyeonjong <hj109gim@gmail.com>
2015-10-08 23:02:56 +0900

    add number.py

---
+++ week01/bek9/05-number.py
@@ -0,0 +1,38 @@
+# integer
+bek9 = 109
+n = -100
+z = 0
+
+# float
+pi = 3.14
+f = -0.8
+
+# octal
+o = 0o155
+print(o) # 109
+
+# hexadecimal
+h = 0x6D
+print(h) # 109
+
+# complex
+c = 2+3j
+print(c.real) # 2.0
+print(c.imag) # 3.0
+print(c.conjugate()) # (2-3j)
+print(abs(c)) # 3.605551275463989
+
+# calc
+print(bek9 + n) # 9
+print(bek9 * n) # -10900
+print(bek9 / n) # python 2.7 = -2, python3 = -1.09
+
+# exponentiation
+print(2 ** 4) # 16
+
+# remainder
+print(7 % 3) # 1
+print(3 % 7) # 3
+
+print(7 / 4) # 1.75
+print(7 // 4) # 1
